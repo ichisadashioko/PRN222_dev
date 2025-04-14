011ad906cb3c1bad70d60bd4196b60f940591920
shioko <ichisadashioko@gmail.com>
2025-04-14 10:25:49 +0700

    modify dbcontext

--- PRN222_SP25_B5_SL1/Models_DB/Prn222Sp25B5Context.cs
+++ PRN222_SP25_B5_SL1/Models_DB/Prn222Sp25B5Context.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
 namespace PRN222_SP25_B5_SL1.Models_DB;
 
 public partial class Prn222Sp25B5Context : DbContext
 {
+    public static Prn222Sp25B5Context Instance = new Prn222Sp25B5Context();
     public Prn222Sp25B5Context()
     {
+        if(Instance == null){Instance = this;}
     }
 
     public Prn222Sp25B5Context(DbContextOptions<Prn222Sp25B5Context> options)
@@ -25,9 +31,19 @@ public partial class Prn222Sp25B5Context : DbContext
 
     public virtual DbSet<Star> Stars { get; set; }
 
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=ACER\\SQLEXPRESS;uid=sa;password=123456;database=PRN222_SP25_B5;Encrypt=True;TrustServerCertificate=True");
+    {
+        var builder = new ConfigurationBuilder();
+        builder.SetBasePath(Directory.GetCurrentDirectory());
+        builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        var configuration = builder.Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("MyCnn"));
+    }
+
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//         => optionsBuilder.UseSqlServer("Server=ACER\\SQLEXPRESS;uid=sa;password=123456;database=PRN222_SP25_B5;Encrypt=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
