0af823e419bf08afac60c5298b8c970ba4a10032
shioko <ichisadashioko@gmail.com>
2025-04-14 10:10:56 +0700

    new project

---
+++ PRN222_SP25_B5_SL1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRN222_SP25_B5_SL1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
