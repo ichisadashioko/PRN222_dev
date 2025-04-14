c33dba8173ef1100a115216fd55c332d7041f4ee
shioko <ichisadashioko@gmail.com>
2025-04-14 10:35:23 +0700

    stash changes

---
+++ PRN222_SP25_B5_SL1/Controllers/LoadDBController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PRN222_SP25_B5_SL1.Models_DB;
+
+namespace PRN222_SP25_B5_SL1.Controllers
+{
+    public class LoadDBController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var st = Prn222Sp25B5Context.Instance.Students.ToList<Student>();
+            //ViewBag.st = st;
+            return View();
+        }
+    }
+}
