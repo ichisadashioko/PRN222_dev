884caa7e7ad7e162d3618ef574ad6651a60da63b
shioko <ichisadashioko@gmail.com>
2025-04-14 11:02:50 +0700

    modify dbcontext

--- PRN222_SP25_B5_SL1/Models_DB/Prn222Sp25B5Context.cs
+++ PRN222_SP25_B5_SL1/Models_DB/Prn222Sp25B5Context.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
 namespace PRN222_SP25_B5_SL1.Models_DB;
 
 public partial class Prn222Sp25B5Context : DbContext
@@ -24,8 +28,17 @@ public partial class Prn222Sp25B5Context : DbContext
     public virtual DbSet<Student> Students { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=ACER\\SQLEXPRESS;uid=sa;password=123456;database=PRN222_SP25_B5;Encrypt=True;TrustServerCertificate=True");
+    {
+        var builder = new ConfigurationBuilder();
+        builder.SetBasePath(Directory.GetCurrentDirectory());
+        builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        var configuration = builder.Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("MyCnn"));
+    }
+
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//         => optionsBuilder.UseSqlServer("Server=ACER\\SQLEXPRESS;uid=sa;password=123456;database=PRN222_SP25_B5;Encrypt=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
