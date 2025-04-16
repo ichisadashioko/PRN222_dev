2ca7aa50fd83fbdef6b1953bbbd7ef0b8f22dbad
shioko <ichisadashioko@gmail.com>
2025-04-16 09:52:22 +0700

    new razor project

---
+++ PRN222_SP25_B5_RAZOR/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PRN222_SP25_B5_RAZOR.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public bool Gender { get; set; }
+
+    public string DepartId { get; set; } = null!;
+
+    public DateOnly? Dob { get; set; }
+
+    public double Gpa { get; set; }
+
+    public virtual Department Depart { get; set; } = null!;
+}
